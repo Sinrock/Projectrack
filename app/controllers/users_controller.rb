8eca4b0ac0133dced3f0a6ea457114a2e5aab91c
Sinrock <mruhaak@gmail.com>
2020-09-23 01:22:19 -0600

    cleaning up code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,20 +3,18 @@ class UsersController < ApplicationController
     get "/login" do
        erb :login
    end 
-
-
+   
    post "/login" do
       user = User.find_by(email: params[:email])
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
         redirect "/users/#{user.id}"
       else
-        #show error
         redirect "/login"
       end
    end
 
    get "/users/:id" do
-     "users show page"
+     "This would be the users account page view!  Almost there, keep going!!!"
    end
 end
