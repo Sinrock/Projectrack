7cc66ea025e74f8e4ea9230f41b89af869f1c5d4
Sinrock <mruhaak@gmail.com>
2020-09-24 01:45:21 -0600

    added single and all views to projects and projects controller has been updated as well

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,12 @@
 class ProjectsController < ApplicationController
 
+get '/projects' do
+    @projects = Project.all
+    erb :'projects/index'
+end
 
-
-
-
-
+get '/projects/:id' do
+    @project = Project.find(params[:id])
+    erb :"/projects/show"
+end
 end
