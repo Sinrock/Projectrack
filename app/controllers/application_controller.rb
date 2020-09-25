13cc2521cba1ac3ab003f461a7754329b3d4ac93
Sinrock <mruhaak@gmail.com>
2020-09-24 23:43:46 -0600

    fixed projects new page while need to add update and delete then style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,13 +18,13 @@ class ApplicationController < Sinatra::Base
   end
 
   helpers do
+    def logged_in?
+      !!current_user
+    end
   
     def current_user
     User.find_by(id: session[:user_id])
     end
-
-    def logged_in?
-      !!current_user
-    end
+    
   end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,8 +5,35 @@ get '/projects' do
     erb :'projects/index'
 end
 
+get "/projects/new" do
+    erb :"projects/new"
+end
+
+post "/projects" do
+    project = Project.create(
+        title: params[:title],
+        description: params[:description],
+        difficulty: params[:difficulty],
+        duration: params[:duration],
+        tools: params[:tools],
+        parts: params[:parts],
+        location: params[:location],
+        completion: params[:completion],
+        cost: params[:cost],
+        user_id: current_user.id
+    )
+    redirect "/projects/#{project.id}"
+end
+
 get '/projects/:id' do
     @project = Project.find(params[:id])
     erb :"/projects/show"
 end
+
+#update
+    #edit button that takes us to the form
+    #render an edit form
+    #patch method/route that will update the projects
+
+#delete
 end
